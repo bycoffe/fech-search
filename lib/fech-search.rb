2d12e186e17eb99cdd13ed4249e5487b0e3098fa
Aaron Bycoffe <bycoffe@gmail.com>
2013-05-29 16:32:45 -0400

    Move SearchResult into its own file

--- lib/fech-search.rb
+++ lib/fech-search.rb
@@ -1,2 +1,4 @@
+require "fech"
 require "fech-search/version"
 require "fech-search/search"
+require "fech-search/search_result"
--- lib/fech-search/search.rb
+++ lib/fech-search/search.rb
@@ -1,47 +1,6 @@
-require 'fech'
 require 'net/http'
-require 'optparse'
 
 module Fech
-
-  # Fech:SearchResult is a class representing a search result
-  # from Fech::Search.
-  class SearchResult
-    attr_reader :committee_name, :committee_id, :filing_id, :form_type, :period, :date_filed, :description, :amended_by
-
-    # @param [Hash] attrs The attributes of the search result.
-    def initialize(attrs)
-      @date_format = '%m/%d/%Y'
-
-      @committee_name = attrs[:committee_name]
-      @committee_id   = attrs[:committee_id]
-      @filing_id      = attrs[:filing_id]
-      @form_type      = attrs[:form_type]
-      @period         = parse_period(attrs[:period])
-      @date_filed     = Date.strptime(attrs[:date_filed], @date_format)
-      @description    = attrs[:description]
-      @amended_by     = attrs[:amended_by]
-    end
-
-    # Parse the string representing a filing period.
-    # @param [String] period a string representing a filing period
-    # @return [Hash, nil] a hash representing the start and end
-    # of a filing period.
-    def parse_period(period)
-      return if period.nil?
-      from, to = period.split('-')
-      from = Date.strptime(from, @date_format)
-      to = Date.strptime(to, @date_format)
-      {:from => from, :to => to}
-    end
-
-    # The Fech filing object for this search result
-    # @return [Fech::Filing]
-    def filing(opts={})
-      @filing ||= Fech::Filing.new(self.filing_id, opts)
-    end
-  end
-
 
   # Fech::Search is an interface for the FEC's electronic filing search
   # (http://www.fec.gov/finance/disclosure/efile_search.shtml)
